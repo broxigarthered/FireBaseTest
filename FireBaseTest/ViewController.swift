c2827e106b57e1e94b405c42a8f373758f50e929
NikiMusala <nikolay.dutskinov@musala.com>
2018-10-24 16:35:19 +0300

    Initial Commit

---
+++ FireBaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireBaseTest
+//
+//  Created by Nikolay N. Dutskinov on 24/10/2018.
+//  Copyright © 2018 Nikolay N. Dutskinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
